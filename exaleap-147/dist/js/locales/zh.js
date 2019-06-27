d8b062a386280156db6fc58aaa56df1efe80829c
林剑敏 <>
2019-06-27 16:03:43 +0800

    上传资源

---
+++ exaleap-147/dist/js/locales/zh.js
@@ -0,0 +1,52 @@
+window.localString = {
+    'main-title': '千兆跃',
+    'sub-title': 'E氪智慧楼宇企业管理平台',
+    'staff': '员工',
+    'visitor': '访客',
+    'total.parking.space': '总车位',
+    'remaining.parking.space': '空余车位',
+
+    'work.order.today': '本日工单',
+    'work.order.unprocessed': '未处理',
+    'work.order.trend': '近期工单趋势',
+
+    'passenger.flow.today': '本日客流',
+    'passenger.flow.temp': '临时访客',
+    'passenger.flow.failure': '进入失败',
+    'passenger.flow.peak': '高峰时段',
+
+    'fire.alarm': '消防报警',
+    'fire.today.alarm': '本日报警次数',
+    'fire.undeal': '待处理',
+    'fire.deal': '已处理',
+
+    'monitor.area': '重点区域监控',
+
+    'parking.space.statistics': '停车场车位统计',
+    'parking.hours': '营业时间',
+    'parking.used': '已使用车位',
+    'equipment.frequent.visitor': '常访客车位',
+    'equipment.very.visitor': '非常访客车位',
+
+    'gate': '闸机',
+    'elevator': '电梯',
+    'escalator': '扶梯',
+    'alarm': '警报',
+    'passenger.flow': '客流',
+
+    'setting': '设置',
+    'reduction': '还原',
+    'detail': '详情',
+    'switch': '切换',
+
+    'panel.name': '面板名称',
+    'sort': '排序',
+    'panel.work': '工单面板',
+    'panel.visit': '客流量面板',
+    'panel.fire': '消防报警面板',
+    'panel.monitor': '重点区域监控面板',
+    'panel.park': '车位统计面板',
+    'panel.build': '功能面板',
+
+    'confirm': '确认'
+};
