0d6df138b6f9f90037f39f4852c87748c5d6f81a
张亚奇 <yaqi.zhang@exaleap.ai>
2019-07-15 17:01:45 +0800

    替换翻译的文件

--- exaleap-130/dist/js/locales/tw.js
+++ exaleap-130/dist/js/locales/tw.js
@@ -1,3 +1,105 @@
 window.localString = {
-    
+    'wfc-title': '智慧企業管理平台',
+
+    'work.order.today': '本日工單',
+
+    'work.order.unprocessed': '未處理',
+    'work.order.trend': '近期工單趨勢',
+
+    'passenger.flow.today': '本日客流',
+    'passenger.flow.temp': '臨時訪客',
+    'passenger.flow.failure': '進入失敗',
+    'passenger.flow.peak': '高峰時段',
+
+    'heat.distributed': '熱力分布',
+
+    'monitor.area': '重點區域監控',
+
+    'equipment.info': '關鍵設備信息',
+    'equipment.total': '總數',
+    'equipment.online': '在線',
+    'equipment.offline': '離線',
+    'equipment.error': '故障',
+    'equipment.time': '運行時間',
+
+    'cdwp': '冷卻水泵',
+    'chiller': '製冷機',
+    'ahu': '空調機',
+    'chwp': '冷凍水泵',
+    'ct': '冷卻塔',
+    'smoked': '煙感器',
+    'pau': '新風機組',
+    'camera': '攝像頭',
+    'fastgate': '閘機',
+    'bottomSup': '商場',
+    'elevator': '電梯',
+    'alarm': '警報',
+
+    'west-building': '西樓',
+    'east-building': '東樓',
+    'ground-floor': '地面層',
+
+    'density': '能耗密度',
+    'energy': '塔樓公區能耗數據',
+    'today.energy': '本日能耗',
+    'west.energy': '西樓能耗',
+    'east.energy': '東樓能耗',
+
+    'setting': '設置',
+    'reduction': '還原',
+    'detail': '詳情',
+    'switch': '切換',
+
+    'panel.name': '面板名稱',
+    'sort': '排序',
+    'panel.work': '工單面板',
+    'panel.thermal': '熱力分布面板',
+    'panel.monitor': '重點區域監控面板',
+    'panel.equipment': '設備信息面板',
+    'panel.build': '功能面板',
+
+    'confirm': '確認',
+    'msg-btn': '信息按鈕',
+    'main-title': 'ExaLeap',
+    'sub-title': 'EKr智慧企業管理平台',
+    'staff': '員工',
+    'visitor': '訪客',
+    'total.parking.space': '總車位',
+    'remaining.parking.space': '空餘車位',
+
+    'work.order.today': '本日工單',
+    'work.order.unprocessed': '未處理',
+    'work.order.trend': '近期工單趨勢',
+
+    'fire.alarm': '消防報警',
+    'fire.today.alarm': '本日報警次數',
+    'fire.undeal': '待處理',
+    'fire.deal': '已處理',
+
+    'monitor.area': '重點區域監控',
+
+    'parking.space.statistics': '停車場車位統計',
+    'parking.hours': '營業時間',
+    'parking.used': '已使用車位',
+    'equipment.frequent.visitor': '常訪客車',
+    'equipment.very.visitor': '普通訪客車',
+
+    'gate': '閘機',
+    'escalator': '扶梯',
+    'passenger.flow': '客流',
+
+
+    'panel.work': '工單面板',
+    'panel.visit': '客流量面板',
+    'panel.fire': '消防報警面板',
+    'panel.park': '車位統計面板',
+
+    'panel.floorArea': '楼层面积',
+    'panel.floorCarPlate': '车场总车位数',
+    'panel.emptyCarPlate': '空余车位',
+    'panel.dayEnterTime': '日入场车次',
+    'panel.dayChargeMoney': '日实收车费',
+    'panel.recentLeaveing': '最近离开车辆',
+    'panel.monthRent': '月租车',
+
 };
--- exaleap-130/dist/js/locales/zh.js
+++ exaleap-130/dist/js/locales/zh.js
@@ -1,17 +1,75 @@
 window.localString = {
+    'wfc-title': '千兆跃E氪智慧楼宇企业管理平台',
+
+    'work.order.today': '本日工单',
+
+    'work.order.unprocessed': '未处理',
+    'work.order.trend': '近期工单趋势',
+
+    'passenger.flow.today': '本日客流',
+    'passenger.flow.temp': '临时访客',
+    'passenger.flow.failure': '进入失败',
+    'passenger.flow.peak': '高峰时段',
+
+    'heat.distributed': '热力分布',
+
+    'monitor.area': '重点区域监控',
+
+    'equipment.info': '关键设备信息',
+    'equipment.total': '总数',
+    'equipment.online': '在线',
+    'equipment.offline': '离线',
+    'equipment.error': '故障',
+    'equipment.time': '运行时间',
+
+    'cdwp': '冷却水泵',
+    'chiller': '冷机',
+    'ahu': '空调',
+    'chwp': '冷冻水泵',
+    'ct': '冷却塔',
+    'smoked': '烟感器',
+    'pau': '新风',
+    'camera': '摄像头',
+    'fastgate': '闸机',
+    'bottomSup': '底商',
+    'elevator': '电梯',
+    'alarm': '警报',
+
+    'west-building': '西楼',
+    'east-building': '东楼',
+    'ground-floor': '地面层',
+
+    'density': '能耗密度',
+    'energy': '塔楼公区能耗数据',
+    'today.energy': '本日能耗',
+    'west.energy': '西楼能耗',
+    'east.energy': '东楼能耗',
+
+    'setting': '设置',
+    'reduction': '还原',
+    'detail': '详情',
+    'switch': '切换',
+
+    'panel.name': '面板名称',
+    'sort': '排序',
+    'panel.work': '工单面板',
+    'panel.thermal': '热力分布面板',
+    'panel.monitor': '重点区域监控面板',
+    'panel.equipment': '设备信息面板',
+    'panel.build': '功能面板',
+
+    'confirm': '确认',
+    'msg-btn': '信息按钮',
     'main-title': '千兆跃',
-    'sub-title': '智慧企业管理平台',
+    'sub-title': 'E氪智慧楼宇企业管理平台',
+    'staff': '员工',
+    'visitor': '访客',
     'total.parking.space': '总车位',
     'remaining.parking.space': '空余车位',
 
     'work.order.today': '本日工单',
     'work.order.unprocessed': '未处理',
     'work.order.trend': '近期工单趋势',
-
-    'equipment.overhaul': '设备检修',
-    'equipment.pending': '本月待检设备数',
-    'equipment.checked': '本月已检设备数',
-    'equipment.waiting.repair': '本月待维修设备数',
 
     'fire.alarm': '消防报警',
     'fire.today.alarm': '本日报警次数',
@@ -27,24 +85,20 @@ window.localString = {
     'equipment.very.visitor': '非常访客车',
 
     'gate': '闸机',
-    'elevator': '电梯',
     'escalator': '扶梯',
-    'alarm': '警报',
     'passenger.flow': '客流',
 
-    'setting': '设置',
-    'reduction': '还原',
-    'detail': '详情',
-    'switch': '切换',
 
-    'panel.name': '面板名称',
-    'sort': '排序',
     'panel.work': '工单面板',
-    'panel.equipment.overhaul': '设备检修面板',
+    'panel.visit': '客流量面板',
     'panel.fire': '消防报警面板',
-    'panel.monitor': '重点区域监控面板',
     'panel.park': '车位统计面板',
-    'panel.build': '功能面板',
 
-    'confirm': '确认'
+    'panel.floorArea': '楼层面积',
+    'panel.floorCarPlate': '车场总车位数',
+    'panel.emptyCarPlate': '空余车位',
+    'panel.dayEnterTime': '日入场车次',
+    'panel.dayChargeMoney': '日实收车费',
+    'panel.recentLeaveing': '最近离开车辆',
+    'panel.monthRent': '月租车',
 };
